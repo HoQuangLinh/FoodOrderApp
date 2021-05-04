73d5aa4aa1aca70e4e1625d1337525f43fd9bf98
HoQuangLinh <hoquanglinh2710@gmail.com>
2021-05-04 21:18:34 +0700

    Upload file for user

--- middleware/multer.js
+++
@@ -1,8 +0,0 @@
-const multer = require("multer");
-
-const path = require("path");
-
-const storage = multer.memoryStorage();
-const multerUploads = multer({ storage }).single("image");
-
-module.exports = { multerUploads };
